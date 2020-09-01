66c64dba95de5741a0e42fd05247450906551bed
Jeffrey Sewell <jeffrey@urdnot.com>
2020-09-01 13:12:59 -0700

    Broke out case class into a trait

---
+++ src/main/scala/com/urdnot/utils/FutureDataStructures.scala
@@ -0,0 +1,12 @@
+package com.urdnot.utils
+
+trait FutureDataStructures {
+  case class structuredString(
+                               firstWord: String,
+                               secondWord: String,
+                               thirdWord: String,
+                               fourthWord: String,
+                               fifthWord: Int
+                             )
+
+}
--- src/main/scala/com/urdnot/utils/RunFuture.scala
+++ src/main/scala/com/urdnot/utils/RunFuture.scala
@@ -3,32 +3,21 @@ package com.urdnot.utils
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.util.{Failure, Success}
 
-object RunFuture extends App {
+object RunFuture extends App with FutureDataStructures {
   /*
   First, setup a sample list of strings to be parsed. This would normally come from a file or a Kafka topic
    */
   val dataString = List("parse, me, by, csv,0", "parse, me, by, csv, 1", "parse, me, by, csv, 2")
 
   /*
-  The target case class of the structured data
-   */
-
-  case class structuredString(
-                               firstWord: String,
-                               secondWord: String,
-                               thirdWord: String,
-                               fourthWord: String,
-                               fifthWord: Int
-                             )
-
-  /*
   call the object methods with the data and print the reply.
   Because we're using Future the order of the prints is not guaranteed
    */
   FutureMethods.parseStringFuture(dataString).onComplete {
-  case Success(x) => {
-    println(s"Future: ${x}")
-  }
+  case Success(x) =>
+    println {
+      s"Future: $x"
+    }
   case Failure(e) => println("Failed to run the future: " + e.printStackTrace())
   }
 
